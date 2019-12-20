96c169926923bf3b9ecfd2afa1a6b5b8717f25d8
Joshua Forman <cyborgforman@gmail.com>
2019-12-20 08:43:41 -0800

    add first test for equivalent words by name

---
+++ spec/word_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'word'
+
+describe('.word') do
+
+  before(:each) do
+    @word1 = Word.new('trick', nil)
+    @word2 = Word.new('text', nil)
+    @word1.save
+    @word2.save
+  end
+
+  after(:each) do
+    Word.clear
+  end
+
+  describe ('#==') do
+    it('two words are equal if they have the same name') do
+      word3 = Word.new('trick', nil)
+      expect(@word1).to(eq(word3))
+    end
+  end
+
+end
