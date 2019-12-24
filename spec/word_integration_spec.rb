51bf4b6aec37ba6490d4586d19f5bda5bd32f47c
Joshua Forman <cyborgforman@gmail.com>
2019-12-24 15:12:37 -0800

    add integration test for adding a definition

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -11,4 +11,16 @@ describe('create a word path', {:type => :feature}) do
     click_on('Go!')
     expect(page).to have_content('dapper')
   end
+
+  describe('create a definition path', {:type => :feature}) do
+    it('creates a definition and sees it posted on the words page') do
+      word = Word.new('dapper', nil)
+      word.save
+      visit("/dictionary/#{word.id}")
+      fill_in('definition_name', :with => 'fancy')
+      click_on('Add Definition')
+      expect(page).to have_content('fancy')
+    end
+  end
+
 end
